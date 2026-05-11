4bd4fe1a481703c0413d571a5f557f5420470c11
SUMIT-KUMAR-SHAKYA <sumitshakyayt@gmail.com>
2026-05-11 12:16:05 +0530

    url changed

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -3,7 +3,6 @@
  */
 import { io } from 'socket.io-client';
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://neontalk-backend-lcpk.onrender.com";
-export const socket = io(BACKEND_URL, {
+export const socket = io("https://neontalk-backend-lcpk.onrender.com", {
   transports: ["websocket"]
 });
